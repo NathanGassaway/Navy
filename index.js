6dc1e8e3361af556075404263e664c947c189b8b
nathan <nathangway@gmail.com>
2015-11-29 23:45:49 -0500

    fixing?

--- index.js
+++ index.js
@@ -1,44 +1,53 @@
-// $(document).ready(function(){
+$(document).ready(function(){
 
-// 	$(".shower-manual").click(function(){
-// 		$(".title").hide();
-// 		$(".introduction").show();
-// 	});
+	// $(".shower-manual").click(function(){
+	// 	$(".title").hide();
+	// 	$(".introduction").show();
+	// });
 
-// 	var text = ["yes", "no", "Maybe"],
-// 		counter = 0; 
+	var text = ["Prepare shower with soap, shampoo, and a towel.", "After preparing supplies, remove clothing.", "Open curtain and step into shower.",
+	 "Turn on shower head. The shock of cold water is very short-lived.", "For 15 seconds, coat body with water. Use hands or a washcloth to fully douse body.",
+	  "Turn off water and continue to lather skin for 30 seconds.", "Rinse off the soap and shampoo with water for 20 more seconds.", "Turn off water and remove excess soap. Step out of shower. "],
+		counter = -1; 
 
-// 		console.log(text[counter]);
+	$('.forward-arrow').click(function(){
+		$(".intro").hide();
+		$(".section-1").show();
+		counter = (counter + 1) % text.length;
 
-// 	$('.forward-arrow').click(function(){
-// 		counter = (counter + 1) % text.length;
+		$(".instruction-text").text(text[counter])
+	});
 
-// 		$("p").text(text[counter])
+	$('.forward-arrow2').click(function(){
+		counter = (counter + 1) % text.length;
 
-// 	});
-// 	$('.backward-arrow').click(function(){
-// 		counter = (counter - 1) % text.length;
+		$(".instruction-text").text(text[counter])
 
-// 		$("p").text(text[counter])
+	});
+	$('.backward-arrow2').click(function(){
+		counter = (counter - 1) % text.length;
 
-// 	});
-// });
+		$(".instruction-text").text(text[counter])
 
-// // function startTimer(duration, display){
-// 	// 	var timer = duration, seconds;
-// 	// 	setInterval(function() {
-// 	// 		seconds = parseInt(timer % 60, 10);
+	});
 
-// 	// 		seconds = seconds < 10 ? "0" + seconds : seconds;
+});
 
-// 	// 		display.text(seconds);
+// function startTimer(duration, display){
+	// 	var timer = duration, seconds;
+	// 	setInterval(function() {
+	// 		seconds = parseInt(timer % 60, 10);
 
-// 	// 		if(--timer < 0) {
-// 	// 			timer = duration;
-// 	// 		}
-// 	// 	}, 1000);
-// 	// }
+	// 		seconds = seconds < 10 ? "0" + seconds : seconds;
+
+	// 		display.text(seconds);
+
+	// 		if(--timer < 0) {
+	// 			timer = duration;
+	// 		}
+	// 	}, 1000);
+	// }
 	
-// 	// display = $('#time');
-// 	// 	startTimer(30, display)
-// 	// });
+	// display = $('#time');
+	// 	startTimer(30, display)
+	// });
