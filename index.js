55a58ca189a2d2dabaf4dfb038e9484e5a21ef0c
nathan <nathangway@gmail.com>
2015-11-29 22:30:08 -0500

    trying multi-page sites

--- index.js
+++ index.js
@@ -1,35 +1,44 @@
-$(document).ready(function(){
+// $(document).ready(function(){
 
+// 	$(".shower-manual").click(function(){
+// 		$(".title").hide();
+// 		$(".introduction").show();
+// 	});
+
+// 	var text = ["yes", "no", "Maybe"],
+// 		counter = 0; 
+
+// 		console.log(text[counter]);
+
+// 	$('.forward-arrow').click(function(){
+// 		counter = (counter + 1) % text.length;
+
+// 		$("p").text(text[counter])
+
+// 	});
+// 	$('.backward-arrow').click(function(){
+// 		counter = (counter - 1) % text.length;
+
+// 		$("p").text(text[counter])
+
+// 	});
+// });
+
+// // function startTimer(duration, display){
+// 	// 	var timer = duration, seconds;
+// 	// 	setInterval(function() {
+// 	// 		seconds = parseInt(timer % 60, 10);
+
+// 	// 		seconds = seconds < 10 ? "0" + seconds : seconds;
+
+// 	// 		display.text(seconds);
+
+// 	// 		if(--timer < 0) {
+// 	// 			timer = duration;
+// 	// 		}
+// 	// 	}, 1000);
+// 	// }
 	
-	$(".forward-arrow").click(function(){
-		$(".introduction").hide()
-		$(".section-1").show();
-
-	});
-
-	// var array = {
-	// 	1: fucked up beyond reason,
-	// 	2: hell yah,
-	// 	3: whoa,
-	// }
-	
-});
-
-// function startTimer(duration, display){
-	// 	var timer = duration, seconds;
-	// 	setInterval(function() {
-	// 		seconds = parseInt(timer % 60, 10);
-
-	// 		seconds = seconds < 10 ? "0" + seconds : seconds;
-
-	// 		display.text(seconds);
-
-	// 		if(--timer < 0) {
-	// 			timer = duration;
-	// 		}
-	// 	}, 1000);
-	// }
-	
-	// display = $('#time');
-	// 	startTimer(30, display)
-	// });
+// 	// display = $('#time');
+// 	// 	startTimer(30, display)
+// 	// });
