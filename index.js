794dbb4611cdccec3fed281c31b87a39cb2f6ac9
nathan <nathangway@gmail.com>
2015-12-03 10:28:15 -0500

    major changes to document and cocomposition

--- index.js
+++ index.js
@@ -1,35 +1,98 @@
 $(document).ready(function(){
 
-	// $(".shower-manual").click(function(){
-	// 	$(".title").hide();
-	// 	$(".introduction").show();
-	// });
-
-	var text = ["Prepare shower with soap, shampoo, and a towel.", "After preparing supplies, remove clothing.", "Open curtain and step into shower.",
-	 "Turn on shower head. The shock of cold water is very short-lived.", "For 15 seconds, coat body with water. Use hands or a washcloth to fully douse body.",
-	  "Turn off water and continue to lather skin for 30 seconds.", "Rinse off the soap and shampoo with water for 20 more seconds.", "Turn off water and remove excess soap. Step out of shower. "],
-		counter = -1; 
-
-	$('.forward-arrow').click(function(){
-		$(".intro").hide();
+	// var text = ["Prepare shower with soap, shampoo, and a towel.", "", "After preparing supplies, remove clothing.", "", "Open curtain and step into shower.", "",
+	//  "Turn on shower head. The shock of cold water is very short-lived.", "", "For 15 seconds, coat body with water. Use hands or a washcloth to fully douse body.", "",
+	//   "Turn off water and continue to lather skin for 30 seconds.", "", "Rinse off the soap and shampoo with water for 20 more seconds.", "", "Turn off water and remove excess soap. Step out of shower.", ""],
+		
+		$("cover", "#first-image", "#second-step").hide();
+			$(document).keydown(function(e){
+				//forward
+    if (e.keyCode === 39) {
+    	$("#introduction").hide();
 		$(".section-1").show();
-		counter = (counter + 1) % text.length;
 
-		$(".instruction-text").text(text[counter])
+
+	counter++;
+	if(counter < $(".description").length) $(".description").hide();
+	$('.description').eq(counter).show();
+	console.log(counter);
+	if(counter === 10){
+		document.getElementById('showervideo').play();
+		$(".forward-arrow2").hide();
+		$(".backward-arrow2").hide();
+		setInterval(function(){
+			$(".forward-arrow2").show();
+			$(".backward-arrow2").show();
+		}, 10000);
+	} else {
+		document.getElementById('showervideo').pause();
+	}
+
+    	// counter = (counter + 1) % text.length;
+
+		// if(counter === 3){
+		// 	var x = document.createElement("img");
+		// 	x.setAttribute("src", "../duck.png");
+		// 	document.body.appendChild(x);
+		// } else{
+		// 	$("img").remove();
+		// }
+
+             // $(".instruction-text").text(text[counter])
+    }
+         //backwards
+    if (e.keyCode === 37) {
+    		counter--;
+	if(counter < $(".description").length) $(".description").hide();
+	$('.description').eq(counter).show();
+	if(counter === 10){
+		document.getElementById('showervideo').play();
+	} else {
+		document.getElementById('showervideo').pause();
+	}
+
+  //   	if(counter === 3){
+		// 	var x = document.createElement("img");
+		// 	x.setAttribute("src", "../duck.png");
+		// 	document.body.appendChild(x);
+		// } else{
+		// 	$("img").remove();
+		// }
+  //            $(".instruction-text").text(text[counter])
+         }
 	});
+	var counter = -1
+	$('.forward-arrow').click(function(){
+		$("#introduction").hide();
+		$(".section-1").show();
 
+	counter++;
+	if(counter < $(".description").length) $(".description").hide();
+	$('.description').eq(counter).show();
+
+		// counter = (counter + 1) % text.length;
+
+		// $(".instruction-text").text(text[counter])
+	});
 	$('.forward-arrow2').click(function(){
-		counter = (counter + 1) % text.length;
-
-		$(".instruction-text").text(text[counter])
+	
+	counter++;
+	if(counter < $(".description").length) $(".description").hide();
+	$('.description').eq(counter).show();
 
 	});
 	$('.backward-arrow2').click(function(){
-		counter = (counter - 1) % text.length;
+		counter--;
+	if(counter < $(".description").length) $(".description").hide();
+	$('.description').eq(counter).show();
+		if(counter < 0){
+			return; //also need to stop counter
+		}
 
+		$(".title-1").text(counter)
 		$(".instruction-text").text(text[counter])
-
 	});
+
 
 });
 
