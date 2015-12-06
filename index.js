6319ccc4b0b4cfe44a5900a223219cef8a7d8c58
nathan <nathangway@gmail.com>
2015-12-05 21:46:21 -0500

    lotta work done on functions

--- index.js
+++ index.js
@@ -1,116 +1,216 @@
-$(document).ready(function(){
+      $(document).ready(function(){
+  
+  var forward = document.getElementById("forward"),
+  backward = document.getElementById("backward"),
+  openDoc = document.getElementById("cover"),
+  openIntro = document.getElementById("intro-paragraph"),
+  countdown = document.getElementById("seventh-image"),
+  hideVideo, 
+  myTimer,
+  counter = 0;
 
-	// var text = ["Prepare shower with soap, shampoo, and a towel.", "", "After preparing supplies, remove clothing.", "", "Open curtain and step into shower.", "",
-	//  "Turn on shower head. The shock of cold water is very short-lived.", "", "For 15 seconds, coat body with water. Use hands or a washcloth to fully douse body.", "",
-	//   "Turn off water and continue to lather skin for 30 seconds.", "", "Rinse off the soap and shampoo with water for 20 more seconds.", "", "Turn off water and remove excess soap. Step out of shower.", ""],
-		
-		$("cover", "#first-image", "#second-step").hide();
-			$(document).keydown(function(e){
-				//forward
-    if (e.keyCode === 39) {
-    	$("#introduction").hide();
-		$(".section-1").show();
+  $("cover", "#first-image", "#second-step").hide();
 
+  function clearcoverPage(){
+    $("#cover").hide();
+    $(".introduction-paragraph").show();
+  }
 
-	counter++;
-	if(counter < $(".description").length) $(".description").hide();
-	$('.description').eq(counter).show();
-	console.log(counter);
-	if(counter === 10){
-		document.getElementById('showervideo').play();
-		$(".forward-arrow2").hide();
-		$(".backward-arrow2").hide();
-		setInterval(function(){
-			$(".forward-arrow2").show();
-			$(".backward-arrow2").show();
-		}, 10000);
-	} else {
-		document.getElementById('showervideo').pause();
-	}
+  function clearintro(){
+    $(".introduction-paragraph").hide();
+    $("#introduction").hide();
+    $(".section-1").show();
+  }
 
-    	// counter = (counter + 1) % text.length;
+  function nextVideo(){
+    hideVideo = window.setInterval(function(){
+      counter = 11;
+      $(".forward-arrow").show();
+      $(".backward-arrow").show();
+      $(".description").eq(10).hide();
+      $(".description").eq(11).show();
+      $('#shower-video').trigger("pause");
+      }, 1000);
+  }
 
-		// if(counter === 3){
-		// 	var x = document.createElement("img");
-		// 	x.setAttribute("src", "../duck.png");
-		// 	document.body.appendChild(x);
-		// } else{
-		// 	$("img").remove();
-		// }
+  function nextAudio(){
+    counter = 15;
+    $(".forward-arrow").show();
+    $(".backward-arrow").show();
+    $(".description").eq(14).hide();
+    $(".description").eq(15).show();
+    $('#shower-audio').trigger("pause");
+  }
 
-             // $(".instruction-text").text(text[counter])
-    }
-         //backwards
-    if (e.keyCode === 37) {
-    		counter--;
-	if(counter < $(".description").length) $(".description").hide();
-	$('.description').eq(counter).show();
-	if(counter === 10){
-		document.getElementById('showervideo').play();
-	} else {
-		document.getElementById('showervideo').pause();
-	}
+  // function startTimer(duration, display){
+  //      var timer = duration, seconds;
+  //      myTimer = setInterval(function() {
+  //        seconds = parseInt(timer % 60, 10);
 
-  //   	if(counter === 3){
-		// 	var x = document.createElement("img");
-		// 	x.setAttribute("src", "../duck.png");
-		// 	document.body.appendChild(x);
-		// } else{
-		// 	$("img").remove();
-		// }
-  //            $(".instruction-text").text(text[counter])
-         }
-	});
-	var counter = -1
-	$('.forward-arrow').click(function(){
-		$("#introduction").hide();
-		$(".section-1").show();
+  //        seconds = seconds < 10 ? "0" + seconds : seconds;
 
-	counter++;
-	if(counter < $(".description").length) $(".description").hide();
-	$('.description').eq(counter).show();
+  //        display.text(seconds);
 
-		// counter = (counter + 1) % text.length;
+  //        if(--timer < 0) {
+  //          timer = 00;
+  //          nextAudio();
+  //        }
+  //      }, 1000); 
+  //      display = $('#shower-timer');
+  //      startTimer(20, display);
+  //    }
 
-		// $(".instruction-text").text(text[counter])
-	});
-	$('.forward-arrow2').click(function(){
-	
-	counter++;
-	if(counter < $(".description").length) $(".description").hide();
-	$('.description').eq(counter).show();
+function clearNextSlide(){
+    clearInterval(hideVideo);
+  }
 
-	});
-	$('.backward-arrow2').click(function(){
-		counter--;
-	if(counter < $(".description").length) $(".description").hide();
-	$('.description').eq(counter).show();
-		if(counter < 0){
-			return; //also need to stop counter
-		}
+  function clearotherSlide(){
+    clearInterval(myTimer);
+  }
 
-		$(".title-1").text(counter)
-		$(".instruction-text").text(text[counter])
-	});
+  function forward(){
+    counter++;
+    if(counter < $(".description").length) $(".description").hide();
+    $('.description').eq(counter).show();
+    console.log(counter);
+  }
+
+  function showerVideo(){
+    if(counter === 10){
+      $('#shower-video').trigger("play");
+      $(".forward-arrow").hide();
+      $(".backward-arrow").hide();
+      nextVideo();
+    } else {
+        $('#shower-video').trigger("pause");
+        clearNextSlide();
+      }
+  }
+  function showerAudio(){
+    if (counter === 14) {
+      $('#shower-audio').trigger("play");
+      $(".forward-arrow").hide();
+      $(".backward-arrow").hide();
+      function startTimer(duration, display){
+          var timer = duration, seconds;
+          setInterval(function() {
+            seconds = parseInt(timer % 60, 10);
+
+            seconds = seconds < 10 ? "0" + seconds : seconds;
+
+            display.text(seconds);
+
+            if(--timer < 0) {
+              timer = 00,
+              nextAudio();
+            }
+          }, 1000);
+      	}
+        
+        display = $('#shower-timer');
+          startTimer(20, display);
+
+          $("body").mouseover(function(){
+  		setInterval(function(){
+    if($("body").hasClass("gray")){
+      $("body").addClass("blue").removeClass("gray");
+      
+    } else {
+      $("body").addClass("gray").removeClass("blue");
+      
+    	}
+	  }, 1000);
+	}); 
+    } else{
+      $('#shower-audio').trigger("pause");
+      clearotherSlide()
+      
+    };
+  }
+  function colorChange(){
+    setInterval(function(){
+      if($("body").has("gray")){
+        $("body").addClass("blue").removeClass("gray");
+      } else{
+        $("body").addClass("gray").removeClass("blue");
+      }
+    }, 1000);
+  }
+
+  function stopOvertrace(){
+    if(counter > 19){
+        counter = 0;
+      }
+  }
+
+  function stopRetrace(){
+    if(counter < 0){
+        counter = 0;
+      }
+  }
+  // --> this doesn't even need to exist really.  
+  $(document).keydown(function(e){
+        //forward
+      if (e.keyCode === 39) {
+
+        counter++;
+      if(counter < $(".description").length) $(".description").hide();
+      $('.description').eq(counter).show();
+      console.log(counter);
+        
+      clearintro();
+
+      showerVideo();
+
+      showerAudio();
+
+      stopOvertrace();
+
+      }
+           //backwards
+      if (e.keyCode === 37) {
+        counter--;
+      if(counter < $(".description").length) $(".description").hide();
+      $('.description').eq(counter).show();
+
+      showerVideo();
+
+      showerAudio();
+
+      stopRetrace();
+      }
+  });
+  forward.onclick = function(){
+    counter++;
+
+    if(counter < $(".description").length) $(".description").hide();
+    $('.description').eq(counter).show();
+
+    showerVideo();
+
+    showerAudio();
+
+    stopOvertrace();
+  };
+  backward.onclick = function(){
+      counter--;
+      console.log(counter);
+    if(counter < $(".description").length) $(".description").hide();
+    $('.description').eq(counter).show();
+
+    stopRetrace();
+  };
+//show add a "for loop" during the  water phases if someone tries to go backwards.
+  openDoc.onclick = function(){
+    clearcoverPage();
+  };
+
+  openIntro.onclick = function(){
+    counter++;
+    if(counter < $(".description").length) $(".description").hide();
+    $('.description').eq(counter).show();
+    clearintro();
+  };
 
 
 });
-
-// function startTimer(duration, display){
-	// 	var timer = duration, seconds;
-	// 	setInterval(function() {
-	// 		seconds = parseInt(timer % 60, 10);
-
-	// 		seconds = seconds < 10 ? "0" + seconds : seconds;
-
-	// 		display.text(seconds);
-
-	// 		if(--timer < 0) {
-	// 			timer = duration;
-	// 		}
-	// 	}, 1000);
-	// }
-	
-	// display = $('#time');
-	// 	startTimer(30, display)
-	// });
